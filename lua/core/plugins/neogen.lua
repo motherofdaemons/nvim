3d754b1fc6c7375764b80fb8a0390cd5e7af6bbc
Lilith Screws <lilith@queercoded.io>
2023-01-18 17:18:32 -0500

    Adding leap comment and neogen

---
+++ lua/core/plugins/neogen.lua
@@ -0,0 +1,20 @@
+local M = {
+  "danymat/neogen",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "L3MON4D3/LuaSnip",
+  },
+}
+
+function M.config()
+  require("neogen").setup({ snippet_engine = "luasnip" })
+  local wk = require("which-key")
+  wk.register({
+    d = {
+      name = "Documentation",
+      g = { ":lua require('neogen').generate()<CR>", "Generate" },
+    },
+  }, { prefix = "<leader>" })
+end
+
+return M
--- lua/core/plugins/tree-sitter.lua
+++ lua/core/plugins/tree-sitter.lua
@@ -1,6 +1,25 @@
 local M = {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "mrjones2014/nvim-ts-rainbow",
+  },
   run = ":TSUpdate",
 }
+
+function M.config()
+  require("nvim-treesitter.configs").setup {
+    ensure_installed = "all",
+
+    auto_install = true,
+    highlight = {
+      enable = true,
+    },
+    rainbow = {
+      enable = true,
+      extended_mode = true,
+      max_file_lines = nil,
+    },
+  }
+end
 
 return M
