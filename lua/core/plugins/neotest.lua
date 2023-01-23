24f0020b4acb1190831dfceb5f7fb9c445bb3dcf
Lilith Screws <lilith@queercoded.io>
2023-01-23 14:11:24 -0500

    Be able to run rust test

---
+++ lua/core/plugins/neotest.lua
@@ -0,0 +1,35 @@
+local M = {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "antoinemadec/FixCursorHold.nvim",
+    "rouge8/neotest-rust",
+  }
+}
+
+function M.config()
+  local wk = require("which-key")
+  wk.register({
+    t = {
+      name = "Test",
+      r = {
+        name = "Run",
+        t = { function() require("neotest").run.run() end, "Run test" },
+        f = { function() require("neotest").run.run(vim.fn.expand("%")) end, "Run file" },
+      },
+      o = {
+        name = "Open",
+        o = { function() require("neotest").output.open({ enter = true }) end, "Open output" },
+      },
+    }, prefix = "<leader>"
+  })
+
+  require("neotest").setup({
+    adapters = {
+      require("neotest-rust")
+    }
+  })
+end
+
+return M
