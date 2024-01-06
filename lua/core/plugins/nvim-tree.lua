71fcfc393f4e52331252ade87357501e9b1efa0e
Lilith Screws <lilith@queercoded.io>
2024-01-06 10:31:22 -0800

    Nvim Tree toggle and autoclose

--- lua/core/plugins/nvim-tree.lua
+++ lua/core/plugins/nvim-tree.lua
@@ -4,6 +4,21 @@ local M = {
     "kyazdani42/nvim-web-devicons",
   },
   opts = {},
+  config = function()
+    require('nvim-tree').setup()
+    local wk = require('which-key')
+    wk.register({
+      e = { '<cmd>NvimTreeFindFileToggle<cr>', 'File Tree' },
+    }, { prefix = '<leader>' })
+    vim.api.nvim_create_autocmd("BufEnter", {
+      nested = true,
+      callback = function()
+        if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+          vim.cmd "quit"
+        end
+      end
+    })
+  end,
 }
 
 
